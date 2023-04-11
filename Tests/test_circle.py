0a4361a060c58082bf4b40ef78d4d8201f8a9358
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2023-04-11 12:53:19 +0530

    Update test_circle.py

--- Tests/test_circle.py
+++ Tests/test_circle.py
@@ -1,15 +1,15 @@
-import math
+# import math
 
 
-class Test_Circle:
-    radius = 2
+# class Test_Circle:
+#     radius = 2
 
-    def test_radius(self, radius):
-        radius = 2
-        if radius < 0:
-            raise ValueError("positive radius expected")
-        self.radius = radius
+#     def test_radius(self, radius):
+#         radius = 2
+#         if radius < 0:
+#             raise ValueError("positive radius expected")
+#         self.radius = radius
 
-    def test_area(self):
-        assert self.radius >= 0, "positive radius expected"
-        # assert math.pi * self.radius ** 2
+#     def test_area(self):
+#         assert self.radius >= 0, "positive radius expected"
+#         # assert math.pi * self.radius ** 2
