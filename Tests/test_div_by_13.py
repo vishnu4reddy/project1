01c9b1328b21b82d0ea894b98a8b4d71bf4af765
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2023-04-11 12:52:28 +0530

    Update test_div_by_13.py

--- Tests/test_div_by_13.py
+++ Tests/test_div_by_13.py
@@ -1,5 +1,5 @@
-import pytest
+# import pytest
 
 
-def test_divisible_by_13(input_value):
-    assert input_value % 13 == 0
+# def test_divisible_by_13(input_value):
+#     assert input_value % 13 == 0
